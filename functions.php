a52a9044c74ce5c3a1b5f87adbdd0f0594f05229
Ferdous Ahmed Himel <himibaba10@gmail.com>
2024-09-02 20:00:45 +0600

    Added the additional files and added content on header & footer

--- functions.php
+++ functions.php
@@ -2,7 +2,12 @@
 
 function university_files()
 {
-    wp_enqueue_style("main-file", get_stylesheet_uri());
+    wp_enqueue_style("university_main_styles", get_theme_file_uri("/build/style-index.css"));
+    wp_enqueue_style("university_main_styles", get_theme_file_uri("/build/style-index.css"));
+
+    wp_enqueue_style("google_font", "//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i");
+    wp_enqueue_style("fontawesome_icons", "//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
+
 }
 
 add_action("wp_enqueue_scripts", "university_files");
