be9a983aa40ae461386dc605cb4fe574dfac34e2
Ferdous Ahmed Himel <himibaba10@gmail.com>
2024-09-02 20:18:11 +0600

    Added javascript file in function.php and the home slider is working

--- functions.php
+++ functions.php
@@ -2,13 +2,17 @@
 
 function university_files()
 {
-
+    // CSS files:
     wp_enqueue_style("google_font", "//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i");
     wp_enqueue_style("fontawesome_icons", "//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
 
     wp_enqueue_style("university_main_styles", get_theme_file_uri("/build/style-index.css"));
     wp_enqueue_style("university_extra_styles", get_theme_file_uri("/build/index.css"));
 
+    // JavaScript files:
+
+    // Structure: wp_enqueue_script("nickname", "filepath", "dependency array", "version", "boolean whether the script will run before closing body tag") 
+    wp_enqueue_script("university_main_js", get_theme_file_uri("/build/index.js"), array("jquery"), "1.0", true);
 }
 
 add_action("wp_enqueue_scripts", "university_files");
