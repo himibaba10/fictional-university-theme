ebae57dd40e0f16009a781db9187ad1055d98da4
Ferdous Ahmed Himel <himibaba10@gmail.com>
2024-09-04 20:25:39 +0600

    Added functionality of children navigation links

--- page.php
+++ page.php
@@ -13,20 +13,40 @@ while (have_posts()) {
     </div>
 
     <div class="container container--narrow page-section">
-        <div class="metabox metabox--position-up metabox--with-home-link">
-            <p>
-                <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About
-                    Us</a> <span class="metabox__main">Our History</span>
-            </p>
-        </div>
 
-        <!-- <div class="page-links">
-            <h2 class="page-links__title"><a href="#">About Us</a></h2>
-            <ul class="min-list">
-                <li class="current_page_item"><a href="#">Our History</a></li>
-                <li><a href="#">Our Goals</a></li>
-            </ul>
-        </div> -->
+        <?php
+        // wp_get_post_parent_id takes an id and returns the id of it's parent page. If the page does not have a parent page then it will return 0.
+    
+        //get_the_ID() return the id of the page
+        $theParent = wp_get_post_parent_id(get_the_ID());
+        $parentTitle = $theParent ? get_the_title($theParent) : get_the_title();
+        $parentID = $theParent ?: get_the_ID();
+        $isParent = get_pages(array(
+            "child_of" => get_the_ID()
+        ));
+
+        if ($theParent) { ?>
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p>
+                    <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home"
+                            aria-hidden="true"></i> Back to
+                        <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title(); ?></span>
+                </p>
+            </div>
+        <?php } ?>
+
+        <?php if ($isParent or $theParent) { ?>
+            <div class="page-links">
+                <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent); ?>"><?php echo $parentTitle ?></a>
+                </h2>
+                <ul class="min-list">
+                    <?php wp_list_pages(array(
+                        "title_li" => NULL,
+                        "child_of" => $parentID
+                    )); ?>
+                </ul>
+            </div>
+        <?php } ?>
 
         <div class="generic-content"><?php the_content(); ?></div>
     </div>
