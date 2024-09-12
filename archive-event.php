947eb9880e45a8f0546481e5a2811e5b3aa9f86c
Ferdous Ahmed Himel <himibaba10@gmail.com>
2024-09-13 04:57:58 +0600

    Added functionality to get the future events only on the events archive page

--- archive-event.php
+++ archive-event.php
@@ -13,23 +13,29 @@
 
 <div class="container container--narrow page-section">
     <?php while (have_posts()) {
-        the_post(); ?>
+        the_post();
+        $eventDate = new DateTime(get_field('event_date'));
+        ?>
 
         <div class="event-summary">
             <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-                <span class="event-summary__month"><?php the_time("M") ?></span>
-                <span class="event-summary__day"><?php the_time("d") ?></span>
+                <span class="event-summary__month">
+                    <?php echo $eventDate->format("M") ?>
+                </span>
+                <span class="event-summary__day">
+                    <?php echo $eventDate->format("d") ?>
+                </span>
             </a>
             <div class="event-summary__content">
-                <h5 class="event-summary__title headline headline--tiny"><a
-                        href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                <p>
-                    <?php echo substr(get_the_content(), 0, 50) ?>
-                    <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a>
-                </p>
+                <h5 class="event-summary__title headline headline--tiny">
+                    <a href="<?php the_permalink(); ?>">
+                        <?php the_title(); ?>
+                    </a>
+                </h5>
+                <?php echo substr(get_the_content(), 0, 50) ?>
+                <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a>
             </div>
         </div>
-
     <?php }
     echo paginate_links(); ?>
 </div>
--- functions.php
+++ functions.php
@@ -28,3 +28,22 @@ function university_features()
 
 // after_setup_theme is for adding extra features like adding tab title
 add_action("after_setup_theme", "university_features");
+
+function university_adjust_queries($query)
+{
+    if (!is_admin() and is_post_type_archive("event") and $query->is_main_query()) {
+        $query->set("meta_key", "event_date");
+        $query->set("orderby", "meta_value");
+        $query->set("order", "ASC");
+        $query->set("meta_query", array(
+            array(
+                "key" => "event_date",
+                "compare" => ">=",
+                "value" => current_time("Ymd"),
+                "type" => "numeric"
+            )
+        ));
+    }
+}
+
+add_action("pre_get_posts", "university_adjust_queries");
