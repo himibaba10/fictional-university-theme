f049bbaeeea2790639d83957cb876a86b50fb07a
Ferdous Ahmed Himel <himibaba10@gmail.com>
2024-09-14 18:53:27 +0600

    Created single program page

--- single-program.php
+++ single-program.php
@@ -7,15 +7,18 @@ while (have_posts()) {
             style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>)"></div>
         <div class="page-banner__content container container--narrow">
             <h1 class="page-banner__title"><?php the_title(); ?></h1>
+            <div class="page-banner__intro">
+                <p>THIS WILL BE A SUBTITLE</p>
+            </div>
         </div>
     </div>
 
     <div class="container container--narrow page-section">
         <div class="metabox metabox--position-up metabox--with-home-link">
             <p>
-                <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link("event"); ?>">
+                <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link("program"); ?>">
                     <i class="fa fa-home" aria-hidden="true"></i>
-                    Events Home
+                    All Programs
                 </a>
                 <span class="metabox__main"><?php the_title(); ?></span>
             </p>
