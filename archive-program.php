ab77d14763dce16c20cc968baa72dce3d458145c
Ferdous Ahmed Himel <himibaba10@gmail.com>
2024-09-15 00:41:59 +0600

    Created the program archive page

---
+++ archive-program.php
@@ -0,0 +1,44 @@
+<?php get_header(); ?>
+
+<div class="page-banner">
+    <div class="page-banner__bg-image"
+        style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>)"></div>
+    <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title">All Events</h1>
+        <div class="page-banner__intro">
+            <p>See what is going on in our world.</p>
+        </div>
+    </div>
+</div>
+
+<div class="container container--narrow page-section">
+    <ul class="link-list min-list">
+        <?php while (have_posts()) {
+            the_post(); ?>
+            <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+        <?php }
+        echo paginate_links(); ?>
+    </ul>
+</div>
+
+<?php get_footer(); ?>
+
+<!-- 
+Notes about the functions of this page: 
+have_posts() & the_post() = used in while loop to show the posts
+
+the_archive_title() = shows the archive page title
+the_archive_description() = shows the archive page description
+
+the_permalink() = post link
+the_title() = post title
+the_excerpt() = post excerpt
+the_date() = shows only one post of the same day (Should not use for that)
+the_time() = shows the date of the posts
+
+the_author() = Displays just the author's name.
+the_author_link() = Displays the author's name with a link to their website (if applicable).
+the_author_posts_link() = Displays the author's name with a link to their posts archive page.
+
+paginate_links() = adds pagination (we have to echo)
+-->
