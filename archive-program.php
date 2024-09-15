75a3c8fcb4d10f342e8c6070b845d462989fef36
Ferdous Ahmed Himel <himibaba10@gmail.com>
2024-09-15 20:30:07 +0600

    Fixed some issues like changed title and subtitle of Programs archive

--- archive-program.php
+++ archive-program.php
@@ -4,9 +4,9 @@
     <div class="page-banner__bg-image"
         style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>)"></div>
     <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">All Events</h1>
+        <h1 class="page-banner__title">All Programs</h1>
         <div class="page-banner__intro">
-            <p>See what is going on in our world.</p>
+            <p>There is something for everyone. Have a look around.</p>
         </div>
     </div>
 </div>
