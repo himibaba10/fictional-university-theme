872afca5b67ecd136df13bd2aad690acab35f685
Ferdous Ahmed Himel <himibaba10@gmail.com>
2024-09-15 20:05:08 +0600

    Showed related programs on the single event page

--- single-event.php
+++ single-event.php
@@ -1,7 +1,6 @@
 <?php get_header();
 while (have_posts()) {
     the_post(); ?>
-
     <div class="page-banner">
         <div class="page-banner__bg-image"
             style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>)"></div>
@@ -22,8 +21,25 @@ while (have_posts()) {
         </div>
 
         <div class="generic-content"><?php the_content(); ?></div>
-    </div>
 
+        <?php
+        $relatedPrograms = get_field("related_programs");
+        if ($relatedPrograms) {
+            echo '<hr class="section-break">';
+            echo '<h2 class="headline headline--medium">Related Program(s)</h2>';
+            echo "<ul class='link-list min-list'>";
+            foreach ($relatedPrograms as $program) { ?>
+                <li>
+                    <a href="<?php echo get_the_permalink($program) ?>">
+                        <?php echo get_the_title($program) ?>
+                    </a>
+                </li>
+            <?php }
+            echo "</ul>";
+        }
+        ?>
+    </div>
 <?php }
+
 get_footer();
 ?>
