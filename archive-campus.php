f941e525b19c68c7c2a3ed7cea5beb3ef299ba0d
Ferdous Ahmed Himel <himibaba10@gmail.com>
2024-09-27 00:51:31 +0600

    Created an archive page for campus but could not load google map for billing

---
+++ archive-campus.php
@@ -0,0 +1,38 @@
+<?php get_header();
+pageBanner(array(
+    'title' => 'Our Campuses',
+    'subtitle' => 'We have several conveniently located campuses'
+));
+?>
+
+<div class="container container--narrow page-section">
+    <ul class="link-list min-list">
+        <?php while (have_posts()) {
+            the_post(); ?>
+            <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+        <?php }
+        echo paginate_links(); ?>
+    </ul>
+</div>
+
+<?php get_footer(); ?>
+
+<!-- 
+Notes about the functions of this page: 
+have_posts() & the_post() = used in while loop to show the posts
+
+the_archive_title() = shows the archive page title
+the_archive_description() = shows the archive page description
+
+the_permalink() = post link
+the_title() = post title
+the_excerpt() = post excerpt
+the_date() = shows only one post of the same day (Should not use for that)
+the_time() = shows the date of the posts
+
+the_author() = Displays just the author's name.
+the_author_link() = Displays the author's name with a link to their website (if applicable).
+the_author_posts_link() = Displays the author's name with a link to their posts archive page.
+
+paginate_links() = adds pagination (we have to echo)
+-->
