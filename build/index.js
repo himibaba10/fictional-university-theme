0097743cdeaa45704cf7b34d2eb2475966f9f28b
Ferdous Ahmed Himel <himibaba10@gmail.com>
2024-11-12 13:29:09 +0600

    Search related HTML are not in the footer, they are in the JS file

--- build/index.js
+++ build/index.js
@@ -112,6 +112,7 @@ __webpack_require__.r(__webpack_exports__);
 
 class Search {
   constructor() {
+    this.addSearchHTML();
     this.openButton = jquery__WEBPACK_IMPORTED_MODULE_0___default()(".js-search-trigger");
     this.closeButton = jquery__WEBPACK_IMPORTED_MODULE_0___default()(".search-overlay__close");
     this.overlay = jquery__WEBPACK_IMPORTED_MODULE_0___default()(".search-overlay");
@@ -173,6 +174,24 @@ class Search {
             </ul>` : "<p>No general info found.</p>"}
           `);
     });
+  }
+  addSearchHTML() {
+    jquery__WEBPACK_IMPORTED_MODULE_0___default()("body").append(`
+      <div class="search-overlay">
+          <div class="search-overlay__top">
+              <div class="container">
+                  <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+                  <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term"
+                      autocomplete="off">
+                  <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+              </div>
+          </div>
+
+          <div class="container">
+              <div id="search-overlay__results"></div>
+          </div>
+      </div>
+      `);
   }
 }
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Search);
--- src/modules/Search.js
+++ src/modules/Search.js
@@ -2,6 +2,7 @@ import $ from "jquery";
 
 class Search {
   constructor() {
+    this.addSearchHTML();
     this.openButton = $(".js-search-trigger");
     this.closeButton = $(".search-overlay__close");
     this.overlay = $(".search-overlay");
@@ -91,6 +92,25 @@ class Search {
           `);
       }
     );
+  }
+
+  addSearchHTML() {
+    $("body").append(`
+      <div class="search-overlay">
+          <div class="search-overlay__top">
+              <div class="container">
+                  <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+                  <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term"
+                      autocomplete="off">
+                  <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+              </div>
+          </div>
+
+          <div class="container">
+              <div id="search-overlay__results"></div>
+          </div>
+      </div>
+      `);
   }
 }
 
