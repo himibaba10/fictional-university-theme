8897d2af8015a3f4a8a27449e2d001299d3a0099
Ferdous Ahmed <himibaba10@gmail.com>
2025-07-10 17:11:17 +0600

    Added the custom date field for events on homepage

--- front-page.php
+++ front-page.php
@@ -23,11 +23,15 @@
                 "posts_per_page" => 2
             ));
             while ($homeEvents->have_posts()) {
-                $homeEvents->the_post(); ?>
+                $homeEvents->the_post();
+                $eventDate = new DateTime(get_field("event_date"));
+                $eventMonth = $eventDate->format("M");
+                $eventDate = $eventDate->format("d");
+                ?>
                 <div class="event-summary">
                     <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-                        <span class="event-summary__month"><?php the_time("M"); ?></span>
-                        <span class="event-summary__day"><?php the_time("d"); ?></span>
+                        <span class="event-summary__month"><?php echo $eventMonth; ?></span>
+                        <span class="event-summary__day"><?php echo $eventDate; ?></span>
                     </a>
                     <div class="event-summary__content">
                         <h5 class="event-summary__title headline headline--tiny"><a
