da41302e832f17eb90a3586bcc06cb290a88165b
Ferdous Ahmed <himibaba10@gmail.com>
2025-08-07 20:03:37 +0600

    Only upcoming events are now shown in the events archive page

--- archive-event.php
+++ archive-event.php
@@ -13,12 +13,16 @@
 
 <div class="container container--narrow page-section">
     <?php while (have_posts()) {
-        the_post(); ?>
+        the_post();
+        $eventDate = new DateTime(get_field("event_date"));
+        $eventMonth = $eventDate->format("M");
+        $eventDate = $eventDate->format("d");
+        ?>
 
         <div class="event-summary">
             <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-                <span class="event-summary__month"><?php the_time("M"); ?></span>
-                <span class="event-summary__day"><?php the_time("d"); ?></span>
+                <span class="event-summary__month"><?php echo $eventMonth; ?></span>
+                <span class="event-summary__day"><?php echo $eventDate; ?></span>
             </a>
             <div class="event-summary__content">
                 <h5 class="event-summary__title headline headline--tiny"><a
