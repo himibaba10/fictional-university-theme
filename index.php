1f33967e767c2770b9b77490f7b7d27ad76a9b9f
Ferdous Ahmed <himibaba10@gmail.com>
2025-08-16 19:49:07 +0600

    Used pageBanner function on every page

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
-<?php get_header(); ?>
-
-<div class="page-banner">
-    <div class="page-banner__bg-image"
-        style="background-image: url(<?php echo get_theme_file_uri("/images/ocean.jpg") ?>)"></div>
-    <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">Welcome to our blog!</h1>
-        <div class="page-banner__intro">
-            <p>THIS SUBTITLE WILL BE CHANGED LATER</p>
-        </div>
-    </div>
-</div>
+<?php get_header();
+pageBanner(array(
+    "title" => "Welcome to our blog!",
+    "subtitle" => "We have awesome blog writers!"
+));
+?>
 
 <div class="container container--narrow page-section">
     <?php while (have_posts()) {
--- single-professor.php
+++ single-professor.php
@@ -1,21 +1,14 @@
 <?php
 
 get_header();
+pageBanner();
 
 while (have_posts()) {
-    the_post(); ?>
+    the_post();
 
-    <div class="page-banner">
-        <div class="page-banner__bg-image"
-            style="background-image: url(<?php echo get_field("page_banner_image")["sizes"]["pageBanner"]; ?>)">
-        </div>
-        <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title"><?php the_title(); ?></h1>
-            <div class="page-banner__intro">
-                <p><?php the_field("page_banner_subtitle"); ?></p>
-            </div>
-        </div>
-    </div>
+    ?>
+
+
 
     <div class="container container--narrow page-section">
         <div class="generic-content">
